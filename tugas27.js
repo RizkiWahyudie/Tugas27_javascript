24c1f2a6e325fbe20fb9a37d448e28408f43c997
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 22:09:55 +0700

    add file

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+function konversi(){
+  var a = "No ";
+  var b = "rumah saya ";
+  var c = 30;
+  var d = "A";
+  var e = "Jalan pangeran Tirtayasa No ";
+  var f = 70;
+
+  hasil = a + b + Number(c) + d;
+  hasil2 = e + Number(f);
+  console.log(hasil);
+  console.log(hasil2);
+}
+
+konversi();
